378bd2c6605602579d248b05aa818bd26fb054f3
DanielAntunes <antunesdaniel083@gmail.com>
2023-04-26 15:50:09 -0300

    Atualização

--- curso-angular/src/app/components/first-component/first-component.component.ts
+++ curso-angular/src/app/components/first-component/first-component.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.css']
+  styleUrls: ['./first-component.component.css'],
 })
 export class FirstComponentComponent {
-
+  name = 'Daniel';
+  age = 30;
+  job = 'programador';
+  hobbies = ['Correr', 'jogar', 'Estudar'];
+  car = {
+    name: 'Polo',
+    years: 2019,
+  };
 }
